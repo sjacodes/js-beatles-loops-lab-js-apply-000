47ad540e15242f6677d1dec4995ba1b50e664fcb
Sarah Jacob <smjacob91@gmail.com>
2018-06-10 16:30:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i ++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array
+}
 
 
 
