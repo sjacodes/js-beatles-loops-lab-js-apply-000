088e3d57d00d0e3effcf9fc16977ff23861690ef
Sarah Jacob <smjacob91@gmail.com>
2018-06-10 16:33:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,12 @@ function johnLennonFacts(facts) {
   return array;
 }
 
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!"); n++;
+  } 
+  while (n < 15);
+  return array;
+}
 
