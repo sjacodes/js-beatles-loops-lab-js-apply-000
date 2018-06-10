cf69f447d0f0d501144179a2374893f5a5b6d460
Sarah Jacob <smjacob91@gmail.com>
2018-06-10 16:32:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ function johnLennonFacts(facts) {
   var array = [];
   var i = 0;
   while (i < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
+    array.push(facts[i] + "!!!"); i++;
   }
   return array;
 }
